9dc4d7f15b63a4a85609ad2a3da4a3139c320911
adesaputraa <adesaputra0928@gmail.com>
2022-11-01 09:28:02 +0700

    tugas section 10 shop hp

---
+++ Test Suites/DuoApp.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DuoApp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>921c824d-3e5a-4576-942e-beda060ad5d3</testSuiteGuid>
+   <testCaseLink>
+      <guid>3dce87bb-40c9-410a-a4f5-4e17a1e368d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/ChooseTheme</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7469bb6-e9f4-48e2-822c-30929602f257</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OpenDetail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
